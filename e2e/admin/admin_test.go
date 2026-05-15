22c48e2c68fe69c40a47bce3c64c12e4b75b7c3c
fullsend-fix <fullsend-code@users.noreply.github.com>
2026-05-15 15:51:04 +0000

    fix: update e2e test to use new VendorBinary 5-param signature
    Update VendorBinary call in e2e/admin/admin_test.go to pass owner and
    repo as separate arguments, matching the signature change from this PR.
    
    Addresses review feedback on #1011
    
    Signed-off-by: fullsend-fix <fullsend-code@users.noreply.github.com>

--- e2e/admin/admin_test.go
+++ e2e/admin/admin_test.go
@@ -547,7 +547,7 @@ func vendorBinaryForE2E(t *testing.T, env *e2eEnv) {
 	require.NoError(t, err, "building fullsend binary: %s", string(out))
 
 	t.Log("Uploading vendored binary to .fullsend/bin/fullsend...")
-	err = layers.VendorBinary(context.Background(), env.client, testOrg, tmpBinary.Name())
+	err = layers.VendorBinary(context.Background(), env.client, testOrg, forge.ConfigRepoName, tmpBinary.Name())
 	require.NoError(t, err, "vendoring binary")
 	t.Log("Vendored binary uploaded successfully")
 }
